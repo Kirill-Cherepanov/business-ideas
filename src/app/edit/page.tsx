b77675ed484867da417c915d74467b04d2b5b128
Cherepanov Kirill <cherkirr@gmail.com>
2023-07-16 12:16:11 +0300

    feat: style /edit page

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -2,15 +2,17 @@
 
 import { useEffect, useState } from 'react';
 import { redirect, useSearchParams } from 'next/navigation';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 import { Form } from '@/components';
-import { TIdea } from '@/types/custom';
+import { FormattedIdea, TIdea } from '@/types/custom';
 import { deleteIdea, editIdea, getIdea } from '@/utils';
 
 const UpdatePrompt = () => {
   const ideaId = useSearchParams().get('id');
 
-  const [idea, setIdea] = useState<TIdea | null>(null);
+  const [idea, setIdea] = useState<FormattedIdea | null>(null);
 
   const formSumbitHandler = (idea: Omit<TIdea, 'creator'>) => editIdea(ideaId || '', idea);
   const deleteHandler = () => {
@@ -30,12 +32,29 @@ const UpdatePrompt = () => {
   if (!ideaId) return null;
 
   return (
-    <Form
-      type="Edit"
-      initIdea={idea}
-      handleSubmit={formSumbitHandler}
-      handleDelete={deleteHandler}
-    />
+    <>
+      <Box marginTop={8} marginBottom={4}>
+        <Typography
+          component="h2"
+          variant="h2"
+          sx={{
+            background: 'linear-gradient(to right,#f03070, #a60ca6)',
+            backgroundClip: 'text',
+            color: 'transparent',
+            fontWeight: '900',
+          }}
+        >
+          Edit your idea
+        </Typography>
+      </Box>
+
+      <Form
+        type="Edit"
+        initIdea={idea}
+        handleSubmit={formSumbitHandler}
+        handleDelete={deleteHandler}
+      />
+    </>
   );
 };
 
